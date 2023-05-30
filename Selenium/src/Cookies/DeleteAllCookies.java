1433578aabd698f0056079c2b27ed6b10ce88b86
MAYUR <MAYUR@MAYUR>
2023-05-30 09:56:51 +0530

    This is 4th Commit

---
+++ Selenium/src/Cookies/DeleteAllCookies.java
@@ -0,0 +1,30 @@
+package Cookies;
+
+import java.util.Set;
+
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.netty.util.collection.CharObjectHashMap;
+
+public class DeleteAllCookies {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.fb.com");
+		
+		Set<Cookie> allCookies = driver.manage().getCookies();
+		System.out.println(allCookies.size());
+		for (Cookie singleCookie : allCookies) {
+			System.out.println(singleCookie.getName());
+		}
+		
+		driver.manage().deleteAllCookies();
+		
+		allCookies = driver.manage().getCookies();
+		System.out.println(allCookies.size());
+		for (Cookie singleCookie : allCookies) {
+			System.out.println(singleCookie.getName());
+		}
+	}
+}
