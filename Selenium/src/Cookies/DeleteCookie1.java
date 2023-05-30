46e6da79a7c9843b34ef0396c2c2187962983860
MAYUR <MAYUR@MAYUR>
2023-05-30 10:07:28 +0530

    30-1

--- Selenium/src/Cookies/DeleteCookie1.java
+++ Selenium/src/Cookies/DeleteCookie1.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class DeleteCookie {
+public class DeleteCookie1 {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.fb.com");
